8ed29ac194e2c0ab269de5097ac5b22dec1164d9
Naresh Kumar Vaid <nkraj021@gmail.com>
2024-11-17 16:11:59 +0530

    responsive widget added

--- lib/domain/constants/responsive_ui/responsive_app.dart
+++ lib/domain/constants/responsive_ui/responsive_app.dart
@@ -5,8 +5,8 @@ class ResponsiveApp extends StatelessWidget {
   final Widget desktop;
 
   const ResponsiveApp({super.key, required this.desktop, required this.mobile});
-  // using media query get current platform screen width/height
 
+  // using media query get current platform screen width/height
   static Size mqResponsive(BuildContext context) {
     return MediaQuery.sizeOf(context);
   }
@@ -25,6 +25,26 @@ class ResponsiveApp extends StatelessWidget {
       },
     );
   }
+}
 
-  /// using orientiation builder
+/// using orientiation builder
+class AppOrientiation extends StatelessWidget {
+  const AppOrientiation(
+      {super.key, required this.portrait, required this.landscape});
+
+  final Widget portrait;
+  final Widget landscape;
+
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        if (orientation == Orientation.portrait) {
+          return portrait;
+        } else {
+          return landscape;
+        }
+      },
+    );
+  }
 }
